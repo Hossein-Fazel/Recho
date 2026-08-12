569fa210f400d2892d88e92662bdaf2273756b70
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-08-12 17:05:09 +0330

    feat: add user schema

---
+++ internal/infra/postgres/migrations/000001_users.up.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    username CITEXT NOT NULL UNIQUE,
+    email CITEXT NOT NULL UNIQUE,
+    
+    display_name VARCHAR(100),
+    avatar_url TEXT,
+    bio VARCHAR(250),
+
+    password_hash TEXT,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT username_length
+        CHECK (char_length(username) BETWEEN 3 AND 30)
+);
