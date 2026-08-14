462b916192ca7954f0a40cd7ecddbfe78fced071
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-08-14 09:31:25 +0330

    feat: add user model

---
+++ internal/model/user.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID          uuid.UUID
+	Username    string
+	DisplayName string
+	AvatarURL   string
+	Bio         string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
