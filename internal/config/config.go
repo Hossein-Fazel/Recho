0e6a688d3ad2cad088d84da10fc7d9294cfde455
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-08-15 11:58:40 +0330

    feat: add config for tokens

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"github.com/Hossein-Fazel/Recho/internal/infra/auth"
 	db "github.com/Hossein-Fazel/Recho/internal/infra/postgres"
 )
 
 type Config struct {
-	DB db.Config `envPrefix:"POSTGRES_"`
+	DB    db.Config `envPrefix:"POSTGRES_"`
+	Token auth.Config `envPrefix:"TOKEN_"`
 }
