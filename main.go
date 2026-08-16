3b64053c81728768eff082da1bd191d6a1e04604
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-08-16 09:02:03 +0330

    fix: fix logger null problem

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 
+	"github.com/Hossein-Fazel/Recho/cmd"
 	"github.com/Hossein-Fazel/Recho/pkg"
 )
 
 func main() {
 	pkg.InitLogger(slog.LevelInfo)
-	fmt.Println("I'm Recho.")
+	pkg.Logger.Info("hello")
+	cmd.Run()
 }
