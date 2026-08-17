19bf22afcb00a8b8c5708d3dc5daf5792b212dfb
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-08-17 18:14:54 +0330

    refactor: refactor user errors

--- internal/adapter/postgres/user.go
+++ internal/adapter/postgres/user.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 
+	"github.com/Hossein-Fazel/Recho/internal/apperr"
 	"github.com/Hossein-Fazel/Recho/internal/infra/postgres/sqlc"
 	"github.com/Hossein-Fazel/Recho/internal/model"
 	"github.com/Hossein-Fazel/Recho/pkg"
@@ -13,9 +16,8 @@ import (
 
 var (
 	URLogger = pkg.Logger.With("component", "userRepo")
-
-	ErrInvalidParams = errors.New("invalid username or password")
 )
+
 type User struct {
 	sql *sqlc.Queries
 }
@@ -30,13 +32,31 @@ func NewUserRepo(sql *sqlc.Queries) *User {
 func (u *User) Create(ctx context.Context, username, passHash string) (*model.User, error) {
 	URLogger.Info("Creating user", "username", username)
 	if username == "" || passHash == "" {
-		return nil, ErrInvalidParams
+		return nil, apperr.InvalidInput("user repo", "invalid username or password", nil)
 	}
 
 	user, err := u.sql.CreateUser(ctx, sqlc.CreateUserParams{
-		Username: username,
+		Username:     username,
 		PasswordHash: passHash,
 	})
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if !errors.As(err, &pgErr) {
+			return nil, apperr.Internal("user repo", err)
+		}
+
+		switch pgErr.Code {
+		case "23505": // unique_violation
+			return nil, apperr.Conflict(
+				"user repo",
+				"re already exists",
+				err,
+			)
+		default:
+			return nil, apperr.Internal("user repo", err)
+		}
+	}
 
 	return &model.User{
 		ID:          user.ID,
@@ -46,13 +66,25 @@ func (u *User) Create(ctx context.Context, username, passHash string) (*model.Us
 		Bio:         user.Bio.String,
 		CreatedAt:   user.CreatedAt,
 		UpdatedAt:   user.UpdatedAt,
-	}, err
+	}, nil
 }
 
 func (u *User) GetByUsername(ctx context.Context, username string) (*model.User, error) {
 	URLogger.Info("Getting user by username", "username", username)
 	user, err := u.sql.GetUserByUsername(ctx, username)
 
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, apperr.NotFound(
+				"user repo",
+				"user not found",
+				err,
+			)
+		}
+
+		return nil, apperr.Internal("user repo", err)
+	}
+
 	return &model.User{
 		ID:          user.ID,
 		Username:    user.Username,
@@ -62,29 +94,24 @@ func (u *User) GetByUsername(ctx context.Context, username string) (*model.User,
 		Bio:         user.Bio.String,
 		CreatedAt:   user.CreatedAt,
 		UpdatedAt:   user.UpdatedAt,
-	}, err
+	}, nil
 }
 
 func (u *User) GetByID(ctx context.Context, id uuid.UUID) (*model.User, error) {
 	URLogger.Info("Getting user by id", "id", id)
 	user, err := u.sql.GetUserByID(ctx, id)
 
-	return &model.User{
-		ID:          user.ID,
-		Username:    user.Username,
-		PassHash:    user.PasswordHash,
-		DisplayName: user.DisplayName.String,
-		AvatarURL:   user.AvatarUrl.String,
-		Bio:         user.Bio.String,
-		CreatedAt:   user.CreatedAt,
-		UpdatedAt:   user.UpdatedAt,
-	}, err
-}
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, apperr.NotFound(
+				"user repo",
+				"user not found",
+				err,
+			)
+		}
 
-func (u *User) GetForLogin(ctx context.Context, username string) (*model.User, error) {
-	URLogger.Info("Getting user for login", "username", username)
-
-	user, err := u.sql.GetUserByUsername(ctx, username)
+		return nil, apperr.Internal("user repo", err)
+	}
 
 	return &model.User{
 		ID:          user.ID,
@@ -95,12 +122,16 @@ func (u *User) GetForLogin(ctx context.Context, username string) (*model.User, e
 		Bio:         user.Bio.String,
 		CreatedAt:   user.CreatedAt,
 		UpdatedAt:   user.UpdatedAt,
-	}, err
+	}, nil
 }
 
 func (u *User) Exists(ctx context.Context, username string) (bool, error) {
-	URLogger.Info("Checking usermae exists", "username", username)
+	URLogger.Info("Checking username existence", "username", username)
 
 	exists, err := u.sql.UsernameExists(ctx, username)
-	return exists, err
+	if err != nil {
+		return false, apperr.Internal("user repo", err)
+	}
+
+	return exists, nil
 }
