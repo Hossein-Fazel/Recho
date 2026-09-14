b346e3866303309e8a770740a085d543bf6d044c
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-09-14 10:51:57 +0330

    feat: add local storage

--- internal/application/interfaces.go
+++ internal/application/interfaces.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"context"
+	"io"
 	"time"
 
 	"github.com/Hossein-Fazel/Recho/internal/model"
@@ -76,4 +77,21 @@ type AccessToken interface {
 type RefreshToken interface {
 	Generate() (*model.RefreshToken, *model.UserRefreshToken, error)
 	Hash(plainText string) string
+}
+
+// Storage
+
+type Storage interface {
+	Upload(
+		ctx context.Context,
+		file io.Reader,
+		size int64,
+		key string,
+		contentType string,
+		public bool,
+	) (string, error)
+	Delete(ctx context.Context, key string) error
+	GetURL(ctx context.Context, key string) (string, error)
+	GetPresignedURL(ctx context.Context, key string) (string, error)
+	Exists(ctx context.Context, key string) (bool, error)
 }
